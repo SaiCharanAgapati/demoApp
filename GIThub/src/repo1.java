c37853672979aaae64ccabfad8ddd55d731bd65e
SaiCharanAgapati <saicharanagapati1999@gmail.com>
2022-11-09 08:44:18 +0530

    push

---
+++ GIThub/src/repo1.java
@@ -0,0 +1,19 @@
+
+public class repo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<n;i++)
+		{
+			for(int j=1;j<n;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+			System.out.println("new");
+			
+		}
+	}
+
+}
